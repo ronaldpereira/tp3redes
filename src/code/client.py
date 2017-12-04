0cf338a30a6627f5ffa3eae9678a661f840ccd5f
Ronald Pereira <ronald.drp11@gmail.com>
2017-12-04 15:00:23 -0200

    Code commented

--- src/code/client.py
+++ src/code/client.py
@@ -1,39 +1,35 @@
 import sys
 import socket
 
-class ClientInfo:
+class ClientInfo: # Class to represent each client
 	def __init__(self):
-		self.serventIp, self.serventPort = sys.argv[1].split(':')
-		self.serventPort = int(self.serventPort)
-		self.seqNum = 0
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.serventIp, self.serventPort = sys.argv[1].split(':') # Gets the servent ip and port from the command line argument
+		self.serventPort = int(self.serventPort) # Casts the port to be a int
+		self.seqNum = 0 # Attribute to sabe the actual sequence number of the client
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP socket creation
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Option to reuse the socket address to stop giving the error: Adress is already being used when running the program multiple times
 
-class Message:
-	def sendKeyReq(client, message):
-		datagram = (5).to_bytes(2, 'big')
-		datagram += (client.seqNum).to_bytes(4, 'big')
-		datagram += message[0:400].encode('ascii')
-
-		print(datagram)
+class Message: # Class to represent the client message methods
+	def sendKeyReq(client, message): # Method to send a keyReq message to a servent
+		datagram = (5).to_bytes(2, 'big') # Message type
+		datagram += (client.seqNum).to_bytes(4, 'big') # Sequence Number
+		datagram += message[0:400].encode('ascii') # Required Key
 
 		client.sock.sendto(datagram, (client.serventIp, client.serventPort))
 		client.seqNum += 1
 
-		responses = []
+		responses = [] # List to store the responses
 		while 1:
 			try:
 				client.sock.settimeout(4)
 
 				responses.append(client.sock.recvfrom(406)) # 2 (message type size) + 4 (sequence number size) + 400 (info or key size)
 
-			except socket.timeout:
-				if len(responses) == 0:
-					datagram = (5).to_bytes(2, 'big')
-					datagram += (client.seqNum).to_bytes(4, 'big')
-					datagram += message[0:400].encode('ascii')
-
-					print(datagram)
+			except socket.timeout: # If the first timeout occurs
+				if len(responses) == 0: # And none response was received, try again
+					datagram = (5).to_bytes(2, 'big') # Message type
+					datagram += (client.seqNum).to_bytes(4, 'big') # New Sequence Number
+					datagram += message[0:400].encode('ascii') # Required Key
 
 					client.sock.sendto(datagram, (client.serventIp, client.serventPort))
 					client.seqNum += 1
@@ -44,40 +40,36 @@ class Message:
 
 							responses.append(client.sock.recvfrom(406)) # 2 (message type size) + 4 (sequence number size) + 400 (info or key size)
 
-						except socket.timeout:
-							if len(responses) == 0:
+						except socket.timeout: # If the second timeout occurs
+							if len(responses) == 0: # And none response was received
 								print('Nenhuma resposta recebida.')
 								return
 
 							else:
-								Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses)
+								Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses) # If any response was received, print those
 								return
 				else:
-					Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses)
+					Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses) # If any response was received, print those
 					return
 
-	def sendTopoReq(client):
-			datagram = (6).to_bytes(2, 'big')
-			datagram += (client.seqNum).to_bytes(4, 'big')
-
-			print(datagram)
+	def sendTopoReq(client): # Method to send a topoReq message to a servent
+			datagram = (6).to_bytes(2, 'big') # Message type
+			datagram += (client.seqNum).to_bytes(4, 'big') # Sequence Number
 
 			client.sock.sendto(datagram, (client.serventIp, client.serventPort))
 			client.seqNum += 1
 
-			responses = []
+			responses = [] # List to store the responses
 			while 1:
 				try:
 					client.sock.settimeout(4)
 
 					responses.append(client.sock.recvfrom(406)) # 2 (message type size) + 4 (sequence number size) + 400 (info or key size)
 
-				except socket.timeout:
-					if len(responses) == 0:
-						datagram = (6).to_bytes(2, 'big')
-						datagram += (client.seqNum).to_bytes(4, 'big')
-
-						print(datagram)
+				except socket.timeout: # If the first timeout occurs
+					if len(responses) == 0: # And none response was received, try again
+						datagram = (6).to_bytes(2, 'big') # Message type
+						datagram += (client.seqNum).to_bytes(4, 'big') # New Sequence Number
 
 						client.sock.sendto(datagram, (client.serventIp, client.serventPort))
 						client.seqNum += 1
@@ -88,28 +80,28 @@ class Message:
 
 								responses.append(client.sock.recvfrom(406)) # 2 (message type size) + 4 (sequence number size) + 400 (info or key size)
 
-							except socket.timeout:
-								if len(responses) == 0:
+							except socket.timeout: # If the second timeout occurs
+								if len(responses) == 0: # And none response was received
 									print('Nenhuma resposta recebida.')
 									return
 
 								else:
-									Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses)
+									Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses) # If any response was received, print those
 									return
 					else:
-						Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses)
+						Message.printResponses((client.seqNum-1).to_bytes(4, 'big'), responses) # If any response was received, print those
 						return
 
-	def printResponses(seqNum, responses):
-		for response in responses:
-			if response[0][2:6] == seqNum:
+	def printResponses(seqNum, responses): # Method to print the response
+		for response in responses: # For each response received
+			if response[0][2:6] == seqNum: # If the sequence number of the received message is the expected sequence number
 				print((response[0][6:]).decode(), ' ', response[1][0], ':', response[1][1], sep='')
 
 			else:
 				print('Mensagem incorreta recebida de ', response[1][0], ':', response[1][1], sep='')
 
 
-client = ClientInfo()
+client = ClientInfo() # Creates the object to represent the client
 
 print('SERVENT IP:', client.serventIp, '\nSERVENT PORT:', client.serventPort)
 
@@ -117,15 +109,15 @@ while 1:
 	message = input('Insira um comando:\n> ')
 
 	if message[0].upper() == '?':
-		message = message[1:].replace(' ','').replace('\t','')
+		message = message[1:].replace(' ','').replace('\t','') # Replaces the spaces and tabs
 		Message.sendKeyReq(client, message)
 
 	elif message[0].upper() == 'T':
 		Message.sendTopoReq(client)
 
 	elif message[0].upper() == 'Q':
-		client.sock.close()
-		print('Client connection finished.')
+		client.sock.close() # Close the client socket
+		print('Socket do client finalizado com sucesso.')
 		break
 
 	else:
--- src/code/servent.py
+++ src/code/servent.py
@@ -1,183 +1,176 @@
 import sys
 import socket
 
-class ServentInfo:
+class ServentInfo: # Class to represent each servent
 	def __init__(self):
-		self.ip = '127.0.0.1'
-		self.ipBinary = []
+		self.ip = '127.0.0.1' # Local host IP
+		self.ipBinary = [] # List of binary numbers that represents the IP
 		self.ipBinaryConstructor()
-		self.port = int(sys.argv[1])
-		self.keyDictionary = {}
+		self.port = int(sys.argv[1]) # Local port received from command line argument
+		self.keyDictionary = {} # Dictionary of keys given by the input file
 		self.keyDictionaryConstructor(sys.argv[2])
-		print(self.keyDictionary)
 		self.serventListConstructor(sys.argv[3])
-		self.receivedMessagesList = []
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Option to reuse the socket address to stop giving the error: Adress is already being used when running the program multiple times
-		self.sock.bind((self.ip, self.port))
+		self.receivedMessagesList = [] # List to store the messages received by each servent
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP socket creation
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Option to reuse the socket address to stop giving the error: 'Address is already being used' when running the program multiple times
+		self.sock.bind((self.ip, self.port)) # Binds the ip and port
 
-	def ipBinaryConstructor(self):
+	def ipBinaryConstructor(self): # Method to construct the binary ip
 		self.ipBinary.append((127).to_bytes(1, 'big'))
 		self.ipBinary.append((0).to_bytes(1, 'big'))
 		self.ipBinary.append((0).to_bytes(1, 'big'))
 		self.ipBinary.append((1).to_bytes(1, 'big'))
 
-	def keyDictionaryConstructor(self, inputFile):
+	def keyDictionaryConstructor(self, inputFile): # Method to construct the key dictionary from the input file
 		file = open(inputFile, 'r')
 		keys = file.readlines()
 
 		for key in keys:
-			if key[0] != '#':
+			if key[0] != '#': # If the first character from the line isn't a comment (#)
 				key = key.split()
-				self.keyDictionary[key[0]] = str.join(' ',(key[1:]))
+				self.keyDictionary[key[0]] = str.join(' ',(key[1:])) # Save the key and the value in the key Dictionary
 
-	def serventListConstructor(self, servents):
+	def serventListConstructor(self, servents): # Method to construct the servent list
 		command = "[('"
-		command += servents[1:].replace('[',"),('").replace(']','').replace(':',"',")
+		command += servents[1:].replace('[',"),('").replace(']','').replace(':',"',") # Transform the list [127.0.0.1:55555[127.0.0.1:55556]] to [('127.0.0.1',55555),('127.0.0.1',55556)], which is the way that Python deals with list of tuples
 		command += ')]'
-		self.serventList = eval(command)
+		self.serventList = eval(command) # Evaluate the command above
 
-	def checkMessageIsNew(self, recvMsg):
+	def checkMessageIsNew(self, recvMsg): # Method to check if the received message is new in the servent by looking into the receivedMessagesList
 		for savedMsg in self.receivedMessagesList:
 			if savedMsg[0] == recvMsg[8:14] and savedMsg[1] == recvMsg[4:8]:
-				print('Message already received:', recvMsg)
 				return False
 
-		self.receivedMessagesList.append((recvMsg[8:14],recvMsg[4:8]))
+		self.receivedMessagesList.append((recvMsg[8:14],recvMsg[4:8])) # If the message is new, insert the message in the receivedMessagesList
 		return True
 
-class Message:
-	def recvKeyReq(servent, clientAddress, recvMsg):
-		keyFloodMsg = (7).to_bytes(2, 'big')
-		keyFloodMsg += (3).to_bytes(2, 'big')
-		keyFloodMsg += recvMsg[2:6]
-		keyFloodMsg += servent.ipBinary[0]
-		keyFloodMsg += servent.ipBinary[1]
-		keyFloodMsg += servent.ipBinary[2]
-		keyFloodMsg += servent.ipBinary[3]
-		keyFloodMsg += (clientAddress[1]).to_bytes(2, 'big')
-		keyFloodMsg += recvMsg[6:]
-
-		print(keyFloodMsg)
+class Message: # Class to represent the servent message methods
+	def recvKeyReq(servent, clientAddress, recvMsg): # Method to deal with keyReq messages
+		keyFloodMsg = (7).to_bytes(2, 'big') # Message type
+		keyFloodMsg += (3).to_bytes(2, 'big') # TTL
+		keyFloodMsg += recvMsg[2:6] # Sequence Number
+		keyFloodMsg += servent.ipBinary[0] # First byte of the binary client ip
+		keyFloodMsg += servent.ipBinary[1] # Second byte of the binary client ip
+		keyFloodMsg += servent.ipBinary[2] # Third byte of the binary client ip
+		keyFloodMsg += servent.ipBinary[3] # Fourth byte of the binary client ip
+		keyFloodMsg += (clientAddress[1]).to_bytes(2, 'big') # Client port
+		keyFloodMsg += recvMsg[6:] # Required Key
 
 		if servent.checkMessageIsNew(keyFloodMsg):
 
-			if recvMsg[6:].decode() in servent.keyDictionary:
+			if recvMsg[6:].decode() in servent.keyDictionary: # If this servent has the key in his keyDictionary, send a resp to the client
 				Message.sendRespFromKeyReq(servent, clientAddress, recvMsg)
 
 			Message.sendMessageToServentList(servent, keyFloodMsg)
 
-	def recvTopoReq(servent, clientAddress, recvMsg):
-		topoFloodMsg = (8).to_bytes(2, 'big')
-		topoFloodMsg += (3).to_bytes(2, 'big')
-		topoFloodMsg += recvMsg[2:6]
-		topoFloodMsg += servent.ipBinary[0]
-		topoFloodMsg += servent.ipBinary[1]
-		topoFloodMsg += servent.ipBinary[2]
-		topoFloodMsg += servent.ipBinary[3]
-		topoFloodMsg += (clientAddress[1]).to_bytes(2, 'big')
-		topoFloodMsg += (servent.ip).encode('ascii')
+	def recvTopoReq(servent, clientAddress, recvMsg): # Method to deal with topoReq messages
+		topoFloodMsg = (8).to_bytes(2, 'big') # Message type
+		topoFloodMsg += (3).to_bytes(2, 'big') # TTL
+		topoFloodMsg += recvMsg[2:6] # Sequence Number
+		topoFloodMsg += servent.ipBinary[0] # First byte of the binary client ip
+		topoFloodMsg += servent.ipBinary[1] # Second byte of the binary client ip
+		topoFloodMsg += servent.ipBinary[2] # Third byte of the binary client ip
+		topoFloodMsg += servent.ipBinary[3] # Fourth byte of the binary client ip
+		topoFloodMsg += (clientAddress[1]).to_bytes(2, 'big') # Client Port
+		topoFloodMsg += (servent.ip).encode('ascii') # Actual servent ip
 		topoFloodMsg += ':'.encode('ascii')
-		topoFloodMsg += (str(servent.port)).encode('ascii')
-
-		print(topoFloodMsg)
+		topoFloodMsg += (str(servent.port)).encode('ascii') # Actual servent port
 
 		if servent.checkMessageIsNew(topoFloodMsg):
 			Message.sendRespFromTopoReq(servent, clientAddress, recvMsg, topoFloodMsg)
 			Message.sendMessageToServentList(servent, topoFloodMsg)
 
-	def recvKeyFlood(servent, recvMsg):
-		if recvMsg[14:].decode() in servent.keyDictionary:
+	def recvKeyFlood(servent, recvMsg): # Method to deal with keyFlood messages
+		if recvMsg[14:].decode() in servent.keyDictionary: # If the key is in the keyDictionary
 			Message.sendRespFromKeyFlood(servent, recvMsg)
 
 		keyFloodMsg = Message.decrementTTL(recvMsg)
 
-		Message.sendMessageToServentList(servent, keyFloodMsg)
+		if Message.TTLIsValid(keyFloodMsg):
+			Message.sendMessageToServentList(servent, keyFloodMsg)
 
+	def recvTopoFlood(servent, recvMsg): # Method to deal with topoFlood messages
+		topoFloodMsg = (8).to_bytes(2, 'big') # Message type
+		topoFloodMsg += recvMsg[2:4] # TTL
+		topoFloodMsg += recvMsg[4:8] # Sequence Number
+		topoFloodMsg += recvMsg[8:14] # IP and Port from the client
+		topoFloodMsg += recvMsg[14:] # Actual sent info
+		topoFloodMsg += ' '.encode('ascii')
+		topoFloodMsg += (servent.ip).encode('ascii') # Actual servent ip
+		topoFloodMsg += ':'.encode('ascii')
+		topoFloodMsg += (str(servent.port)).encode('ascii') # Actual servent port
 
-	def recvTopoFlood(servent, recvMsg):
-		Message.sendRespFromTopoFlood(servent, recvMsg)
+		Message.sendRespFromTopoFlood(servent, topoFloodMsg)
 
-		topoFloodMsg = Message.decrementTTL(recvMsg)
+		topoFloodMsg = Message.decrementTTL(topoFloodMsg)
 
-		Message.sendMessageToServentList(servent, topoFloodMsg)
+		if Message.TTLIsValid(topoFloodMsg):
+			Message.sendMessageToServentList(servent, topoFloodMsg)
 
-	def sendRespFromKeyReq(servent, clientAddress, recvMsg):
-		newMessage = (9).to_bytes(2, 'big')
-		newMessage += recvMsg[2:6]
-		newMessage += servent.keyDictionary[recvMsg[6:].decode()].encode('ascii')
-
-		print(newMessage)
-		print(clientAddress)
-
-		servent.sock.sendto(newMessage, clientAddress)
-
-	def sendRespFromTopoReq(servent, clientAddress, recvMsg, topoFloodMsg):
-		newMessage = (9).to_bytes(2, 'big')
-		newMessage += recvMsg[2:6]
-		newMessage += topoFloodMsg[14:]
-
-		print(newMessage)
-		print(clientAddress)
+	def sendRespFromKeyReq(servent, clientAddress, recvMsg): # Method to send the resp message to the client from a keyReq
+		newMessage = (9).to_bytes(2, 'big') # Message type
+		newMessage += recvMsg[2:6] # Sequence Number
+		newMessage += servent.keyDictionary[recvMsg[6:].decode()].encode('ascii') # Key value
 
 		servent.sock.sendto(newMessage, clientAddress)
 
-	def sendRespFromKeyFlood(servent, recvMsg):
+	def sendRespFromTopoReq(servent, clientAddress, recvMsg, topoFloodMsg): # Method to send the resp message to the client from a topoReq
+		newMessage = (9).to_bytes(2, 'big') # Message type
+		newMessage += recvMsg[2:6] # Sequence Number
+		newMessage += topoFloodMsg[14:] # Actual topology to reach the actual servent
+
+		servent.sock.sendto(newMessage, clientAddress)
+
+	def sendRespFromKeyFlood(servent, recvMsg): # Method to send the resp message to the client from a keyFlood
 		clientAddress = Message.clientAddressConstructor(recvMsg)
 
-		newMessage = (9).to_bytes(2, 'big')
-		newMessage += recvMsg[4:8]
-		newMessage += servent.keyDictionary[recvMsg[14:].decode()].encode('ascii')
+		respMessage = (9).to_bytes(2, 'big') # Message type
+		respMessage += recvMsg[4:8] # Sequence Number
+		respMessage += servent.keyDictionary[recvMsg[14:].decode()].encode('ascii') # Key value
 
-		print(newMessage)
+		servent.sock.sendto(respMessage, clientAddress)
 
-		servent.sock.sendto(newMessage, clientAddress)
+	def sendRespFromTopoFlood(servent, topoFloodMsg): # Method to send the resp message to the client from a topoFlood
+		clientAddress = Message.clientAddressConstructor(topoFloodMsg)
 
-	def sendRespFromTopoFlood(servent, recvMsg):
-		clientAddress = Message.clientAddressConstructor(recvMsg)
+		respMessage = (9).to_bytes(2, 'big') # Message type
+		respMessage += topoFloodMsg[4:8] # Sequence Number
+		respMessage += topoFloodMsg[14:] # Actual topology to reach the actual servent
 
-		newMessage = (9).to_bytes(2, 'big')
-		newMessage += recvMsg[4:8]
-		newMessage += recvMsg[14:]
-		newMessage += ' '.encode('ascii')
-		newMessage += (servent.ip).encode('ascii')
-		newMessage += ':'.encode('ascii')
-		newMessage += (str(servent.port)).encode('ascii')
+		servent.sock.sendto(respMessage, clientAddress)
 
-		print(newMessage)
-
-		servent.sock.sendto(newMessage, clientAddress)
-
-	def sendMessageToServentList(servent, message):
+	def sendMessageToServentList(servent, message): # Method to send the message to all servents in the servent list
 		for serv in servent.serventList:
 			servent.sock.sendto(message, (serv[0], serv[1]))
 
-	def clientAddressConstructor(message):
+	def clientAddressConstructor(message): # Method to construct the client address to auxiliate the resp message methods
 		clientPort = int.from_bytes(message[12:14], 'big')
 
 		return ('127.0.0.1', clientPort)
 
-	def decrementTTL(message):
-		ttlValue = int.from_bytes(message[2:4], 'big')
+	def decrementTTL(message): # Method to decrement the TTL and return the new message datagram
+		ttlValue = int.from_bytes(message[2:4], 'big') # TTL value
 		ttlValue -= 1
-		newMessage = message[0:2]
-		newMessage += ttlValue.to_bytes(2, 'big')
-		newMessage += message[4:]
+		newMessage = message[0:2] # Message type
+		newMessage += ttlValue.to_bytes(2, 'big') # New TTL value
+		newMessage += message[4:] # Rest of the datagram
 
 		return newMessage
 
+	def TTLIsValid(message): # Method to verify the TTL value
+		return True if int.from_bytes(message[2:4], 'big') > 0 else False
 
-servent = ServentInfo()
+
+servent = ServentInfo() # Creates the servent object
 
 while 1:
-	recvMsg, clientAddress = servent.sock.recvfrom(414) # 2 (message type size) + 2 (TTL size) + 4 (sequence number size) + 4 (IP origin size) + 2 (port origin size) + 400 (info or key size)
-	print(recvMsg, clientAddress)
+	recvMsg, recvAddress = servent.sock.recvfrom(414) # 2 (message type size) + 2 (TTL size) + 4 (sequence number size) + 4 (IP origin size) + 2 (port origin size) + 400 (info or key size)
 
 	if recvMsg[0:2] == (5).to_bytes(2, 'big'):
-		Message.recvKeyReq(servent, clientAddress, recvMsg)
+		Message.recvKeyReq(servent, recvAddress, recvMsg)
 
 	elif recvMsg[0:2] == (6).to_bytes(2, 'big'):
-		Message.recvTopoReq(servent, clientAddress, recvMsg)
+		Message.recvTopoReq(servent, recvAddress, recvMsg)
 
 	elif recvMsg[0:2] == (7).to_bytes(2, 'big'):
 		if servent.checkMessageIsNew(recvMsg):
@@ -186,5 +179,3 @@ while 1:
 	elif recvMsg[0:2] == (8).to_bytes(2, 'big'):
 		if servent.checkMessageIsNew(recvMsg):
 			Message.recvTopoFlood(servent, recvMsg)
-
-	print('')
